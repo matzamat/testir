0eb57e9f59868681da44fb9d43145bef2629af4c
matzamat <48964518+matzamat@users.noreply.github.com>
2023-08-06 18:11:06 +0300

    Update mainwindow.cpp
    connect

--- TESTIR/mainwindow.cpp
+++ TESTIR/mainwindow.cpp
@@ -7,6 +7,13 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->verticalSlider, SIGNAL(valueChanged(int)), this, SLOT(on_verticalSlider_valueChanged(int)));
+    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(on_pushButton_clicked()));
+    connect(ui->radioButton, SIGNAL(clicked()), this, SLOT(on_radioButton_clicked()));
+    connect(ui->radioButton_3, SIGNAL(clicked()), this, SLOT(on_radioButton_3_clicked()));
+    connect(ui->radioButton_2, SIGNAL(clicked()), this, SLOT(on_radioButton_2_clicked()));
+    connect(ui->comboBox, SIGNAL(activated(int)), this, SLOT(on_comboBox_activated(int)));
 }
 
 MainWindow::~MainWindow()
@@ -16,48 +23,31 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_verticalSlider_valueChanged(int value)
 {
-    string stri;
-    stri=to_string(value);
-    QString qstr = QString::fromStdString(stri);
-    ui->textBrowser_2->setText(qstr);
+    ui->textBrowser_2->setText(QString::number(value));
 }
 
 void MainWindow::on_pushButton_clicked()
 {
-    // Обработчик нажатия на кнопку
     ui->textBrowser->setText("Клик по кнопке btnName");
 }
 
 void MainWindow::on_radioButton_clicked()
 {
-    // Обработчик нажатия на radioItem 1 из radioName
     ui->textBrowser->setText("выбран элемент radioItem 1 из radioName");
 }
 
 void MainWindow::on_radioButton_3_clicked()
 {
-    // Обработчик нажатия на radioItem 3 из radioName
     ui->textBrowser->setText("выбран элемент radioItem 3 из radioName");
 }
 
 void MainWindow::on_radioButton_2_clicked()
 {
-    // Обработчик нажатия на radioItem 2 из radioName
     ui->textBrowser->setText("выбран элемент radioItem 2 из radioName");
 }
 
 void MainWindow::on_comboBox_activated(int index)
 {
-    // Обработчик выбора элемента из списка
-    if(index==0){
-        ui->textBrowser->setText("выбран элемент listName из списка listName");
-    } else if(index==1){
-        ui->textBrowser->setText("выбран элемент itemName1 из списка listName");
-    } else if(index==2){
-        ui->textBrowser->setText("выбран элемент itemName2 из списка listName");
-    } else if(index==3){
-        ui->textBrowser->setText("выбран элемент itemName3 из списка listName");
-    } else if(index==4){
-        ui->textBrowser->setText("выбран элемент itemName4 из списка listName");
-    }
+    QString itemName = (index > 0) ? "itemName " + QString::number(index) : "listName";
+    ui->textBrowser->setText("выбран элемент " + itemName + " из списка listName");
 }
